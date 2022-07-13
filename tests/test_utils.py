fede3a563a45cc7d24f4dc0d816d0432c903a939
allburov <allburov@gmail.com>
2022-07-13 20:03:57 +0700

    Document test, remove duplicated data from JSON file

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,9 +6,65 @@ from artifactory_cleanup.rules.utils import (
 )
 
 
-def test_artifacts_list_to_tree():
+def load_artifacts():
+    """
+    test-repo
+        - .
+        - user1
+          - package1
+            - 0.1.2
+              - testing
+                - ee9a7171a4577b0077ba521a2a16411f
+                  - export
+                    - conanfile.py | FILE
+                    - conanmanifest.txt | FILE
+                  - package
+                    - 9bfdcfa2bb925892ecf42e2a018a3f3529826676
+                      - bb158820ff3cdfb0764acd8abd0fcb05
+                        - conan_package.tgz | FILE
+                        - conaninfo.txt | FILE
+                        - conanmanifest.txt | FILE
+                - 6a5ffb1868d640cbcb78c934c4728895
+                  - export
+                    - conanfile.py | FILE
+                    - conanmanifest.txt | FILE
+                  - package
+                    - 9bfdcfa2bb925892ecf42e2a018a3f3529826676
+                      - 53518c74f2e9410bb2f6df473adee01f
+                        - conan_package.tgz | FILE
+                        - conaninfo.txt | FILE
+                        - conanmanifest.txt | FILE
+        - user2
+          - package5
+          - package2
+            - 4.2.0
+              - testing
+                - 18259fd357457eed2a5b2dffede67385
+                  - export
+                    - conanfile.py | FIlE
+                    - conanmanifest.txt | FILE
+                  - package
+                    - 976790fbda33f5619b11df9030fb714af0990694
+                      - 8854919f6b53ccf1c347f5a34b475b6a
+                        - conan_package.tgz | FILE
+                        - conaninfo.txt | FILE
+                        - conanmanifest.txt | FILE
+            - 4.2.1
+              - testing2
+                - 18259fd357457eed2a5b2dffede67385
+                  - package
+                    - 976790fbda33f5619b11df9030fb714af0990694
+                      - e708164fb64d5cce0bef0354b59a17ac
+                      - ba38761e49a42a9a44c8be1e30df4886
+        - user3
+    """
     with open("artifacts_list.json", "r") as fp:
         artifacts_list = json.load(fp)
+    return artifacts_list
+
+
+def test_artifacts_list_to_tree():
+    artifacts_list = load_artifacts()
 
     artifacts_tree = artifacts_list_to_tree(artifacts_list)
 
@@ -25,8 +81,7 @@ def test_artifacts_list_to_tree():
 
 
 def test_folder_artifacts_without_children():
-    with open("artifacts_list.json", "r") as fp:
-        artifacts_list = json.load(fp)
+    artifacts_list = load_artifacts()
 
     artifacts_tree = artifacts_list_to_tree(artifacts_list)
 
