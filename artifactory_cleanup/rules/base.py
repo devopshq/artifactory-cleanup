ddbafc910363bd5bbc43d90ddc672644e86006ad
allburov <allburov@gmail.com>
2022-09-05 15:18:16 +0700

    Extend docstrings in Rule and CleanupPolicy

--- artifactory_cleanup/rules/base.py
+++ artifactory_cleanup/rules/base.py
@@ -6,30 +6,66 @@ from collections import namedtuple
 
 
 class Rule(object):
-    """Parent class. Other rules are inherited from this class."""
+    """
+    Rule contains a logic how we get artifacts to remove.
 
-    artifactory_session = None
-    artifactory_server = None
+    Follow Unix philosophy when you're building new Rule: https://en.wikipedia.org/wiki/Unix_philosophy
+    Make it small and then combine them in CleanupPolicy in more complicated entities.
+    """
+
+    def __init__(self):
+        self.artifactory_session = None
+        self.artifactory_server = None
+        self.today = None
 
     def init(self, artifactory_session, artifactory_server, today):
+        """
+        Init the rule after got all information
+        """
         self.artifactory_session = artifactory_session
         self.artifactory_server = artifactory_server
         self.today = today
 
     def _aql_add_filter(self, aql_query_list):
-        """Executed before an AQL query. Adds the required fields to the query"""
+        """
+        Add filter to `items.find` AQL part.
+
+        Here you can filter artifacts on Artifactory side with AQL:
+        https://www.jfrog.com/confluence/display/JFROG/Artifactory+Query+Language
+
+        It's better to filter out as much as possible with that filter
+        rather than get all artifacts and filter out them in memory because it leads to a heavy response from Artifactory
+
+        Also, you can find any conflicts with others rules here, if they conflict on AQL level
+        """
         return aql_query_list
 
-    def _filter_result(self, result_artifact):
-        """Filters artifacts according to some rule"""
-        return result_artifact
-
     def _aql_add_text(self, aql_text):
-        """Adds text to the end of an AQL query"""
+        """
+        Change AQL text after applying all rules filters
+        """
         return aql_text
 
-    def remove_artifact(self, artifacts, result_artifact):
-        """Displays artifacts that have been filtered"""
+    def _filter_result(self, result_artifact):
+        """
+        Filter artifacts after performing AQL query.
+        To remove artifacts from the list - please use Rule.remove_artifact method in order to log the action as well.
+
+        If you have your own logic - please overwrite the method in your Rule class.
+        Here you can filter artifacts in memory, make additional calls to Artifactory or even call other services!
+
+        :param result_artifact: Filtered artifacts list that we get after filter them with AQL
+        :return List of artifacts that you are going to remove
+        """
+        return result_artifact
+
+    @staticmethod
+    def remove_artifact(artifacts, result_artifact):
+        """
+        Remove and log artifacts
+        :param artifacts: Artifacts to remove
+        :param result_artifact: Artifacts remove from it
+        """
         if not isinstance(artifacts, list):
             artifacts = [artifacts]
         for artifact in artifacts:
@@ -37,7 +73,9 @@ class Rule(object):
             result_artifact.remove(artifact)
 
     def aql_add_filter(self, aql_query_list):
-        """Adds a filter to an AQL query"""
+        """
+        Add filters to `items.find` AQL part
+        """
         print(
             "Add AQL Filter - rule: {} - {}".format(
                 self.__class__.__name__, self.little_doc
@@ -51,7 +89,9 @@ class Rule(object):
         return new_aql_query_list
 
     def aql_add_text(self, aql_text):
-        """Adds some expression to AQL query"""
+        """
+        Adds some expression to AQL query
+        """
         print(
             "Add AQL Text - rule: {} - {}".format(
                 self.__class__.__name__, self.little_doc
@@ -65,7 +105,12 @@ class Rule(object):
         return new_aql_text
 
     def filter_result(self, result_artifacts):
-        """Filters received data from AQL"""
+        """
+        Filter artifacts after performing AQL query
+
+        It's a high level function, if you want to specify your own logic
+        please overwrite in your Rule class `_filter_result` method
+        """
         print(
             "Filter artifacts - rule: {} - {}".format(
                 self.__class__.__name__, self.little_doc
@@ -81,7 +126,9 @@ class Rule(object):
 
     @property
     def little_doc(self):
-        # Cut the docstring to show only the very first important line
+        """
+        Cut the docstring to show only the very first important line
+        """
         docs = [x.strip() for x in self.__doc__.splitlines() if x][0]
         return docs
 
@@ -120,22 +167,22 @@ class CleanupPolicy(object):
 
     """
 
-    def __init__(self, name, *args):
+    def __init__(self, name, *rules):
         if not isinstance(name, str):
             raise Exception(
                 "Bad CleanupPolicy, first argument must be name.\n"
-                "CleanupPolicy argument: name={}, *args={}".format(name, args)
+                "CleanupPolicy argument: name={}, *args={}".format(name, rules)
             )
         self.name = name
-        self.rules = list(args)
+        self.rules = list(rules)
 
         # init object if passed not initialized class
-        # See docstring
+        # for `rules.repo` rule
         for i, rule in enumerate(self.rules):
             if inspect.isclass(rule):
                 self.rules[i] = rule(self.name)
 
-        # Defined in self.init() function
+        # Assigned in self.init() function
         self.artifactory_session = None
         self.artifactory_url = None
         self.today = None
@@ -145,11 +192,7 @@ class CleanupPolicy(object):
 
     def init(self, artifactory_session, artifactory_url, today):
         """
-        Set properties and set them to all rules
-
-        :param artifactory_session:
-        :param artifactory_url:
-        :return:
+        Set properties and apply them to all rules
         """
         self.artifactory_session = artifactory_session
         self.artifactory_url = artifactory_url
@@ -181,8 +224,7 @@ class CleanupPolicy(object):
 
     def get_artifacts(self):
         aql_url = "{}/api/search/aql".format(self.artifactory_url)
-        _art_auth_etc = {"data": self.aql_text}
-        r = self.artifactory_session.post(aql_url, **_art_auth_etc)
+        r = self.artifactory_session.post(aql_url, data=self.aql_text)
         r.raise_for_status()
         content = r.json()
         artifacts = content["results"]
@@ -191,11 +233,20 @@ class CleanupPolicy(object):
         return artifacts
 
     def filter(self, artifacts):
+        """
+        Filter artifacts again all rules
+        """
         for rule in self.rules:
             artifacts = rule.filter_result(artifacts)
         return artifacts
 
     def delete(self, artifact, destroy):
+        """
+        Delete the artifact
+        :param artifact: artifact to remove
+        :param destroy: if False - just log the action, do not actually remove the artifact
+        :return:
+        """
         if artifact["path"] == ".":
             artifact_path = "{repo}/{name}".format(**artifact)
         else:
