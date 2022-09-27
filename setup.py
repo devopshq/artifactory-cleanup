8e54016884edcb9593633feb4aba31204a41d752
allburov <allburov@gmail.com>
2022-09-27 17:52:33 +0700

    Bump to 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="artifactory-cleanup",
-    version="1.0.0",
+    version="1.0.1",
     description="Rules and cleanup policies for Artifactory",
     long_description=long_description,
     long_description_content_type="text/markdown",
