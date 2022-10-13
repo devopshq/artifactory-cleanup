f06b2afc44cd82d65d62cd28c712c74e1ee45201
Seth Stevens <39492441+seth-stvns@users.noreply.github.com>
2022-10-12 23:41:00 -0400

    fix:  KeepLatestVersionNFilesInFolder to return the artifact set properly (#82)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- artifactory_cleanup/rules/keep.py
+++ artifactory_cleanup/rules/keep.py
@@ -143,7 +143,7 @@ class KeepLatestVersionNFilesInFolder(Rule):
     The definition of the version is using regexp. By default ``r'[^ \d][[\._]]()((\d+\.)+\d+)')``
     """
 
-    def __init__(self, count, custom_regexp=r"[^\d][\._]((\d+\.)+\d+)"):
+    def __init__(self, count, custom_regexp=r"([\d]+\.[\d]+\.[\d]+)"):
         self.count = count
         self.custom_regexp = custom_regexp
 
@@ -165,6 +165,11 @@ class KeepLatestVersionNFilesInFolder(Rule):
                 key = path + "/" + name_without_version
                 artifacts_by_path_and_name[key].append(artifactory_with_version)
             else:
+                print(
+                    "Warning: Could not identify version for {}/{}".format(
+                        artifact["path"], artifact["name"]
+                    )
+                )
                 artifacts.keep(artifact)
 
         for artifactory_with_version in artifacts_by_path_and_name.values():
@@ -177,7 +182,11 @@ class KeepLatestVersionNFilesInFolder(Rule):
             if good_artifact_count < 0:
                 good_artifact_count = 0
 
-            good_artifacts = artifactory_with_version[good_artifact_count:]
+            # artifactory_with_version contains list of (Version, Artifact)  pairs
+            # Get the artifacts from that for return to 'keep'
+            good_sets = artifactory_with_version[good_artifact_count:]
+            good_artifacts = [good[1] for good in good_sets]
+
             artifacts.keep(good_artifacts)
 
         return artifacts
--- tests/test_rules_keep.py
+++ tests/test_rules_keep.py
@@ -2,6 +2,7 @@ from artifactory_cleanup.rules import (
     KeepLatestNFiles,
     ArtifactsList,
     KeepLatestNFilesInFolder,
+    KeepLatestVersionNFilesInFolder,
 )
 from tests.utils import makeas
 
@@ -88,4 +89,133 @@ def test_KeepLatestNFilesInFolder():
         {"name": "0.0.1.zip", "path": "folder1"},
         {"name": "0.0.1.zip", "path": "folder2"},
     ]
+    assert makeas(remove_these, expected) == expected
+
+
+def test_KeepLatestVersionNFilesInFolderDefault():
+    data = [
+        {
+            "path": "folder1",
+            "name": "0.0.2.zip",
+            "created": "2021-03-20T13:54:52.383+02:00",
+        },
+        {
+            "path": "folder1",
+            "name": "0.0.1.zip",
+            "created": "2021-03-19T13:53:52.383+02:00",
+        },
+        {
+            "path": "folder2",
+            "name": "0.0.1.zip",
+            "created": "2021-03-20T13:54:52.383+02:00",
+        },
+        {
+            "path": "folder2",
+            "name": "0.0.2.zip",
+            "created": "2021-03-19T13:53:52.383+02:00",
+        },
+        {
+            "path": "folder3",
+            "name": "0.0.1.zip",
+            "created": "2021-03-20T13:54:52.383+02:00",
+        },
+    ]
+
+    artifacts = ArtifactsList.from_response(data)
+
+    remove_these = KeepLatestVersionNFilesInFolder(1).filter(artifacts)
+    expected = [
+        {"name": "0.0.1.zip", "path": "folder1"},
+        {"name": "0.0.1.zip", "path": "folder2"},
+    ]
+    assert makeas(remove_these, expected) == expected
+
+
+def test_KeepLatestVersionNFilesInFolderRegexWork():
+    data = [
+        {
+            "path": "folder1",
+            "name": "0.0.2.zip",
+            "created": "2021-03-20T13:54:52.383+02:00",
+        },
+        {
+            "path": "folder1",
+            "name": "0.0.1.zip",
+            "created": "2021-03-19T13:53:52.383+02:00",
+        },
+        {
+            "path": "folder2",
+            "name": "0.0.1.zip",
+            "created": "2021-03-20T13:54:52.383+02:00",
+        },
+        {
+            "path": "folder2",
+            "name": "0.0.2.zip",
+            "created": "2021-03-19T13:53:52.383+02:00",
+        },
+        {
+            "path": "folder3",
+            "name": "0.0.1.zip",
+            "created": "2021-03-20T13:54:52.383+02:00",
+        },
+        {
+            "path": "folder3",
+            "name": "1.0.1.zip",
+            "created": "2021-03-21T13:54:52.383+02:00",
+        },
+        {
+            "path": "folder3",
+            "name": "1.0.2.zip",
+            "created": "2021-03-21T14:54:52.383+02:00",
+        },
+    ]
+
+    artifacts = ArtifactsList.from_response(data)
+
+    remove_these = KeepLatestVersionNFilesInFolder(
+        1, "[\\d]+\\.([\\d]+\\.[\\d]+)"
+    ).filter(artifacts)
+    expected = [
+        {"name": "0.0.1.zip", "path": "folder1"},
+        {"name": "0.0.1.zip", "path": "folder2"},
+        {"name": "1.0.1.zip", "path": "folder3"},
+    ]
+    assert makeas(remove_these, expected) == expected
+
+
+def test_KeepLatestVersionNFilesInFolderRegexFail():
+    data = [
+        {
+            "path": "folder1",
+            "name": "0.0.2.zip",
+            "created": "2021-03-20T13:54:52.383+02:00",
+        },
+        {
+            "path": "folder1",
+            "name": "0.0.1.zip",
+            "created": "2021-03-19T13:53:52.383+02:00",
+        },
+        {
+            "path": "folder2",
+            "name": "0.0.1.zip",
+            "created": "2021-03-20T13:54:52.383+02:00",
+        },
+        {
+            "path": "folder2",
+            "name": "0.0.2.zip",
+            "created": "2021-03-19T13:53:52.383+02:00",
+        },
+        {
+            "path": "folder3",
+            "name": "0.0.1.zip",
+            "created": "2021-03-20T13:54:52.383+02:00",
+        },
+    ]
+
+    artifacts = ArtifactsList.from_response(data)
+
+    remove_these = KeepLatestVersionNFilesInFolder(
+        1, "[^\\d][\\._]((\\d+\\.)+\\d+)"
+    ).filter(artifacts)
+    expected = []
     assert makeas(remove_these, expected) == expected
